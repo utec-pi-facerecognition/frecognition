38b22ec458a91bfa74ae43cdce311e331be42e25
nutax <40769942+nutax@users.noreply.github.com>
2020-05-18 12:25:44 -0500

    #54 F_identification(created): Function for backend
    #54

---
+++ backend_face_comparation.py
@@ -0,0 +1,12 @@
+def backend_face_comparation(_embeddings_to_compare, _dataset_of_embeddings):
+  identified_faces = []
+  
+  def inline_knn(dot, l_dots, l_name, d_name, k):
+    return [ l_dis['l'] for l_dis in sorted([{'l' : l_dot[l_name], 'd' : (sum([(x-y)**(2) for x, y in zip(dot, l_dot[d_name])]))**(1/2)} for l_dot in l_dots], key=lambda x: x['d'])[0:k] ]
+  
+  for _embedding in _embeddings_to_compare:
+    # IMPORTANTE
+    # Cambiar 'nombre_del_alumno' y 'embedding' por las etiquetas correspondientes del dataset
+    identified_faces.append(inline_knn(_embedding, _dataset_of_embeddings, 'nombre_del_alumno', 'embedding', 1)[0])
+  
+  return identified_faces
